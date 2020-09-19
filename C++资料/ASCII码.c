a7235810a25430fe1cb4e5b1ef67f8238d07df51
TangoWalk <2240905406@qq.com>
2020-09-19 19:01:27 +0800

    C++

---
+++ C++资料/ASCII码.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	char a,b;
+	a = 'A';
+	b = a + 32;
+	printf("结果是%d",b);
+	return 0;
+}
---
+++ C++资料/两数比大小2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	int z
+	if(x > y)z = x;
+	else z = y;
+	return(z);
+}
