c16742f58846900ae11c1bccb20bc1c3cb73cf43
TangoWalk <2240905406@qq.com>
2020-09-30 15:13:42 +0800

    2020/9/30/15/13

---
+++ C语言资料/programs/menu.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+int main()
+{
+    char choice,letter;
+    int year,num,count,score;
+    float average;
+    printf("input 'A' to check the year;\ninput 'B' to translate the letter;\ninput 'C' to compute the average score.\n");
+    scanf("%c",&choice);
+
+   switch(choice)
+  { case 'A':
+      {
+        getchar();
+        printf("input the year:");
+        scanf("%d",&year);
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            printf("%d is a leap year.\n ",year);
+        else printf("%d is not a leap year.\n ",year);
+        break;
+      }
+     case 'B':
+        {
+            getchar();
+            printf("input the letter:");
+            scanf("%c",&letter);
+            if (letter <=64)
+                printf("enter letter error!\n");
+            else if (letter <= 89)
+                {
+                    letter += 32;
+                    printf("It is %c.",letter);
+                }
+            else if (letter <= 122)
+                {
+                    letter -= 32;
+                    printf("It is %c",letter);
+                }
+            else
+                printf("enter letter error!\n");
+            break;
+        }
+
+     case 'C':
+         {
+             getchar();
+             printf("input the amount of students:");
+             scanf("%d",&num);
+             average = 0;
+             for (count=0;count<num;count++)
+             {
+                 printf("input the score of Student %d:",count+1);
+                 scanf("%d",&score);
+                 average += score;
+             }
+             average /= num;
+             printf("The average score is %4.2f.",average);
+             break;
+         }
+     default:  printf("enter data error!\n");
+   }
+   return 0;
+ }
